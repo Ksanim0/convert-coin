02ae1d951b798198d835a6c60d425b84392e0612
Edmilson Siqueira Braz Junior <100867319+Ksanim0@users.noreply.github.com>
2022-11-29 18:28:24 -0300

    concerto erro sequencia imagens

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let input = document.getElementById("botao")
 let select = document.getElementById("select-moedas")
+let primeiro = document.getElementById("selectfirst") 
+
 
 async function converterMoedas(){
 
@@ -14,6 +16,8 @@ async function converterMoedas(){
     let inputMoedas = document.getElementById("input-moedas")
     let textoReal = document.getElementById("texto-real")
 
+    
+
     if(select.value === "US$ Dolar Americano"){
     let valorEmDolares = inputValorReais / dolar
     inputMoedas.innerHTML= valorEmDolares.toLocaleString("en-US", {style: "currency", currency: "USD"})
@@ -24,28 +28,52 @@ async function converterMoedas(){
         inputMoedas.innerHTML=valorEmEuros.toLocaleString("de-DE", {style: "currency", currency: "EUR"})
     }
 
+
+
     textoReal.innerHTML= inputValorReais.toLocaleString("pt-br",{style: "currency", currency: "BRL"})
 }
+function bandeira1() {       
+    let textoMoedas1 = document.getElementById("texto-moedas1")
+    let bandeiraMoedas1 = document.getElementById("bandeira-moedas1")
+
+
+    if (primeiro.value === "R$ Real Brasileiro") {
+        textoMoedas1.innerHTML ="Real Brasileiro"
+        bandeiraMoedas1.src ="imagem/brasil.png"
+    }
+
+    if (primeiro.value === "€ Euro") {
+        textoMoedas1.innerHTML ="Euro"
+        bandeiraMoedas1.src ="imagem/euro.png"
+    }
+
+    if (primeiro.value === "US$ Dolar Americano") {
+        textoMoedas1.innerHTML ="Dolar Americano"
+        bandeiraMoedas1.src ="imagem/eua.png"
+    }
+    converterMoedas()    }
 
 function trocaDeMoeda(){
     let textoMoedas = document.getElementById("texto-moedas")
     let bandeiraMoedas = document.getElementById("bandeira-moedas")
 
+
     if(select.value === "US$ Dolar Americano"){
         textoMoedas.innerHTML ="Dolar Americano"
-        bandeiraMoedas.src ="./img/eua.png"
+        bandeiraMoedas.src ="imagem/eua.png"
     }
     if(select.value ==="€ Euro"){
         textoMoedas.innerHTML ="Euro"
-        bandeiraMoedas.src ="./img/euro.png"
+        bandeiraMoedas.src ="imagem/euro.png"
     }
     if(select.value === "R$ Real Brasileiro"){
         textoMoedas.innerHTML ="Real Brasileiro"
-        bandeiraMoedas.src ="./img/brasil.png"
+        bandeiraMoedas.src ="imagem/brasil.png"
     }
 
     converterMoedas()
 }
 
+select.addEventListener("change", bandeira1)
 botao.addEventListener("click", converterMoedas)
 select.addEventListener("change", trocaDeMoeda)
